e22da2174d19fc42490e4066980528ab025d407b
stephelociraptor <139821295+stephelociraptor@users.noreply.github.com>
2024-01-30 10:12:01 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -35,33 +35,33 @@ public void tester()
     }
   }
 }
+  
 public boolean palindrome(String word)
 {
   //your code here
   String word2 = onlyLetters(word);
-  if ((word2.toLowerCase()).equals(reverse(word2.toLowerCase()))){
+  if ((word2.toLowerCase()).equals(reverse(word2.toLowerCase()))) {
     return true;
   }
   return false;
 }
 public String reverse(String str)
 {
- String sNew = new String();
-    //your code here
-  for (int i = str.length(); i > 0; i--){
-   // if (str.substring(i-1,i) != (" ") && Character.isLetter(str.charAt(i))){
-      sNew = sNew + (str.charAt(i-1));
-     //   }
+  String sNew = new String();
+  //your code here
+  for (int i = str.length(); i > 0; i--) {
+    sNew = sNew + (str.charAt(i-1));
   }
   return sNew;
 }
 
-  public String onlyLetters(String sString){
+public String onlyLetters(String sString) {
   String newS = "";
-  for (int i = 0; i < sString.length(); i++){
-    if (Character.isLetter(sString.charAt(i))){
+  for (int i = 0; i < sString.length(); i++) {
+    if (Character.isLetter(sString.charAt(i))) {
       newS = newS + sString.charAt(i);
     }
   }
   return newS;
 }
+
